e3ab8eea5f5f3b639e1b667bafa8fa5b7f35e74c
Muhammad Maheswara Mahardika <if23.muhamamdmahardika@mhs.ubpkarawang.ac.id>
2024-11-28 19:58:03 +0700

    update

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost'; // Ganti dengan host database Anda
-$user = 'root'; // Ganti dengan username database Anda
-$password = ''; // Ganti dengan password database Anda
-$database = 'omega_express'; // Ganti dengan nama database Anda
+$host = 'localhost';
+$user = 'root';
+$password = '';
+$database = 'omega_express';
 
 // Buat koneksi
 $conn = new mysqli($host, $user, $password, $database);
